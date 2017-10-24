02918600d70bbe8214835289f89199ab59c6bd9c
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-24 10:54:58 -0700

    Initial Commit

---
+++ CatMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CatMap
+//
+//  Created by Aaron Johnson on 2017-10-24.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CatMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatMap
+//
+//  Created by Aaron Johnson on 2017-10-24.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
